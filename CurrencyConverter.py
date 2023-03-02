4e7768ef88cc398e24a54bca3208b66e3ce4ef6a
Olafo0 <89582698+Olafo0@users.noreply.github.com>
2023-03-02 11:57:36 +0000

    Update CurrencyConverter.py

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,9 +1,46 @@
 import tkinter as tk
-
+import csv
 
 def main():
     root = tk.Tk()
     root.geometry("500x500")
+
+    #Reading the database
+    file = open("Task3_data.csv","r")
+    datareader = csv.reader(file, delimiter=",")
+
+    currency_converter_data = []
+
+    for row in datareader:
+        currency_converter_data.append(row)
+
+    recent_values = len(currency_converter_data) - 1
+
+    #GBP TO EUR
+    current_GPT_TO_EUR = currency_converter_data[recent_values][1]
+    print("Current GBP TO EUR value - ",current_GPT_TO_EUR)
+
+    #EUR TO GBP
+    current_EUR_TO_GBP = currency_converter_data[recent_values][2]
+    print("Current EUR TO GBP value - ",current_EUR_TO_GBP)
+
+    #GBP TO AUS
+    current_GBP_TO_AUS = currency_converter_data[recent_values][3]
+    print("Current GBP TO AUS value - ",current_GBP_TO_AUS)
+
+    #AUS TO GBP
+    current_AUS_TO_GBP = currency_converter_data[recent_values][4]
+    print("Current AUS TO GBP value - ",current_AUS_TO_GBP)
+
+    #GBP TO JPY
+    current_GBP_TO_JPY = currency_converter_data[recent_values][5]
+    print("Current GBP TO JPY value - ",current_GBP_TO_JPY)
+
+    #JPY TO GBP
+    current_JPY_TO_GBP = currency_converter_data[recent_values][6]
+    print("Current JPY TO GBP value - ",current_JPY_TO_GBP)
+
+
 
 
     note_label1 = tk.Label(root,font=("arial",12), text="Select what currecny you're transferring the money 'from'")
@@ -182,16 +219,30 @@ def main():
         print()
         print()
 
+    def convert_cur():
+        try:
+            from_number = float(From_entry.get())
+            print(from_number)
+        except ValueError:
+            Warning_error = tk.Label(root, text="Make sure you enter a number in both fields.",fg="Red",font=("arial",12))
+            Warning_error.place(x=120,y=410)
+            From_entry.delete(0,"end")
+
+
+            if GBP_calculation_from == True and EURAUS_calculation_to:
+                print("")
 
     #Entry From
     From_entry = tk.Entry(root)
     From_entry.place(x=200,y=120)
 
-    #Entry To
-    To_entry = tk.Entry(root)
-    To_entry.place(x=200,y=260)
+    #Exchanged To
+    Exchanged_to = tk.Entry(root)
+    Exchanged_to.place(x=200,y=360)
 
-
+    #Convert button
+    Convert_button = tk.Button(root, text="Convert",font=("arial",15),command=convert_cur)
+    Convert_button.place(x=220,y=295)
 
     #selection buttons(from)
     GBP_button_from = tk.Button(root, text="GBP",font=("arial",12),command=GBP_flag_from)
@@ -202,7 +253,6 @@ def main():
     AUS_button_from.place(x=275,y=70)
     JPY_button_from = tk.Button(root, text="JPY",font=("arial",12),command=JPY_flag_from)
     JPY_button_from.place(x=350,y=70)
-
 
     #selection buttons(to)
     GBP_button_to = tk.Button(root, text="GBP",font=("arial",12),command=GBP_flag_to)
@@ -215,8 +265,5 @@ def main():
     JPY_button_to.place(x=350,y=210)
 
     root.mainloop()
-
-
-
 
 main()
